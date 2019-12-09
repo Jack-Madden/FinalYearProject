579c883b133f6fec744e9561c5f4b9b2c22384cd
Jack Madden <46815674+Jack-Madden@users.noreply.github.com>
2019-12-09 10:13:15 +0000

    Add files via upload

---
+++ Samples/Text Files/Chords/I-IV-V_A.rb
@@ -0,0 +1,13 @@
+use_bpm 120
+use_synth :sine
+play [:a3, :cs4, :e4]
+wait 0.5
+play [:a3, :cs4, :e4]
+wait 1.5
+play [:d4, :fs4, :a4]
+wait 0.5
+play [:d4, :fs4, :a4]
+wait 1.5
+play [:e4, :gs4, :b4]
+wait 0.5
+play [:e4, :gs4, :b4]
