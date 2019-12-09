253974b10e1988c883011a828deb40ca32eb468f
Jack Madden <46815674+Jack-Madden@users.noreply.github.com>
2019-12-09 09:39:52 +0000

    Add files via upload

---
+++ Samples/Text Files/Melodies/I-V-vi-IV.rb
@@ -0,0 +1,18 @@
+use_bpm 120
+synth :sine
+play [:c4, :e4, :g4]
+wait 0.5
+play [:c4, :e4, :g4]
+wait 1.5
+play [:a3, :c4, :e4]
+wait 0.5
+play [:a3, :c4, :e4]
+wait 1.5
+play [:f3, :a3, :c4]
+wait 0.5
+play [:f3, :a3, :c4]
+wait 1.5
+play [:g3, :b3, :d4]
+wait 0.5
+play [:g3, :b3, :d4]
+wait 1.5
