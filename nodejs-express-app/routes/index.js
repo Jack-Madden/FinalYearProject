f87a04a53908bb9d18a18bd0ef670e8ea5a510b5
Jack Madden <g00338579@gmit.ie>
2020-03-02 15:37:30 +0000

    Finally fixed pathing issue with the pages

--- nodejs-express-app/routes/index.js
+++ nodejs-express-app/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+var app = express()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Random Music Generator' });
+  res.render('index', {title: 'Random Music Generator'});
 });
 
 module.exports = router;
