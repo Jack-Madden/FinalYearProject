53704beffe3cfc55a79524c36308a72977f8062a
Jack Madden <g00338579@gmit.ie>
2020-05-01 23:43:27 +0100

    Multiple changes since last commit. Notable ones: deleted pages directory since it was made redundant. Added Mongoose and Multer to app.js and created schema for audio files. Created new pages in views.

--- nodejs-express-app/app.js
+++ nodejs-express-app/app.js
@@ -1,18 +1,45 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const app = express();
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const mongoUri = "mongodb+srv://Jack:fortyninetyfive@cluster0-u6teg.mongodb.net/test?retryWrites=true&w=majority";
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const audioStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'audiofiles');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  }
+});
 
-var app = express();
+const checkFileType = (req, file, cb) => {
+  if(
+    file.mimetype === 'audio/wav' ||
+    file.mimetype === 'audio/mpeg' ||
+    file.mimetype === 'audio/ogg'
+  ) {
+    cb(null, true);
+  } 
+  else {
+    cb(null, false);
+  }
+};
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({ storage: audioStorage, fileFilter: checkFileType }).single('audio'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -36,6 +63,14 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+mongoose.connect(mongoUri, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(result => {
+  app.listen(8000);
+})
+.catch(err => {
+  console.log(err);
 });
 
 module.exports = app;
--- nodejs-express-app/routes/index.js
+++ nodejs-express-app/routes/index.js
@@ -1,11 +1,30 @@
-var express = require('express');
-var router = express.Router();
-var path = require('path');
-var app = express()
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {title: 'Random Music Generator'});
+  res.render('index', {title: 'E-Music Box'});
+});
+
+router.get('/intro_page', function (req, res, next) {
+  res.render('intro_page', {title: 'E-Music Box'});
+});
+
+router.get('/upload', function (req, res, next) {
+  res.render('upload', {title: 'Upload'});
+});
+
+router.post('/upload',
+  [
+    body('title')
+    .isString()
+    .trim(),
+  ],
+  uploadController.postAddDrums
+);
+
+router.get('/playback_page', function (req, res, next) {
+  res.render('playback_page', {title: 'Playback'});
 });
 
 module.exports = router;
